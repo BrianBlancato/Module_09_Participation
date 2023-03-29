4946c4a206442f6c023e810b7415e4ed8b757260
BrianAnthony_Blancato <bab3fq@virginia.edu>
2023-03-28 21:45:49 -0400

    M09 Participation

---
+++ participation_09/Hello_World_09.py
@@ -0,0 +1,10 @@
+'''
+This file will print hello world when the fuinction hello world
+'''
+
+def hello_world():
+    '''This file will print hello world when the fuinction hello world'''
+    print('Hello World')
+
+if __name__ == '__main__':
+    hello_world()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Hello_World_09',
+     version='0.1',
+     description='A simple demo',
+     url='https://github.com/BrianBlancato/Module_09_Participation',
+     author='Brian',
+     packages=['participation_09'])
